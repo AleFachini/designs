b90326fed9c385e00d7e68a39fa94584dfdd0297
Alejandro Fachini Colombo <alefachinic@Alejandros-MacBook-Pro.local>
2022-05-30 23:12:00 -0300

    diagonal header with painter

--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -31,3 +31,48 @@ class HeaderBordesRendondeados extends StatelessWidget {
     );
   }
 }
+
+class HeaderDiagonal extends StatelessWidget {
+  HeaderDiagonal({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      child: CustomPaint(
+        painter: _HeaderDiagonalPainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderDiagonalPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint(); //es el lapiz
+
+    //Propiedades
+    paint.color = Color(0xff615aab);
+    paint.style = PaintingStyle.fill; // or .fill
+    paint.strokeWidth = 10;
+
+    final path = new Path(); //es el camino que debe hacer el lapiz
+
+    //Dibujar con el paint y el path
+    //moveTO mueve el lapiz sin apollarlo entonces no traza
+    //lineTo mueve el lapiz trazando el recorrido
+    path.moveTo(0, size.height * 0.35);
+    path.lineTo(size.width, size.height * 0.30);
+    path.lineTo(size.width, 0);
+    path.lineTo(0, 0);
+
+    //ahora le indico al canvas que dibuje
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
