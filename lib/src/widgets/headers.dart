a13440be01c086bb160d6eb94808ea95bcb822d3
Alejandro Fachini Colombo <alefachinic@Alejandros-MacBook-Pro.local>
2022-05-30 21:41:37 -0300

    Headers Cuadrado y Circular

---
+++ lib/src/widgets/headers.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class HeaderCuadrado extends StatelessWidget {
+  HeaderCuadrado({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      color: Color(0xff615aab),
+    );
+  }
+}
+
+class HeaderBordesRendondeados extends StatelessWidget {
+  HeaderBordesRendondeados({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      color: const Color(0xff615aab),
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(50),
+          bottomRight: Radius.circular(50),
+        ),
+      ),
+    );
+  }
+}
