885f1c27a51694d1e88ff47cd36d21e513b85b2a
Alejandro Fachini Colombo <alefachinic@Alejandros-MacBook-Pro.local>
2022-06-08 20:41:30 -0300

    agregado de assets svg y prueba de renderizado

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:designs/src/pages/animaciones_page.dart';
 import 'package:designs/src/labs/circular_progress_page.dart';
 import 'package:designs/src/pages/graficas_circulares_page.dart';
 import 'package:designs/src/pages/headers_page.dart';
+import 'package:designs/src/pages/slideshow_page.dart';
 import 'package:designs/src/retos/cuadrado_animado_page.dart';
 import 'package:flutter/material.dart';
 
@@ -30,7 +31,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: GraficasCircularesPage(),
+      home: SlideShowPage(),
     );
   }
 }
