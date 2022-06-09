4cacfc426708d8a12aca207b98a7955a29af2f59
Alejandro Fachini Colombo <alefachinic@Alejandros-MacBook-Pro.local>
2022-06-09 14:32:24 -0300

    agregado de PageView y Slides Slide Dots y Dot

--- lib/src/pages/slideshow_page.dart
+++ lib/src/pages/slideshow_page.dart
@@ -8,8 +8,96 @@ class SlideShowPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: SvgPicture.asset('assets/svgs/slide-1.svg'),
+        child: Column(
+          children: [
+            Expanded(
+              child: _Slides(),
+            ),
+            _Dots(),
+          ],
+        ),
       ),
     );
+  }
+}
+
+class _Dots extends StatelessWidget {
+  _Dots();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 70,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          _Dot(),
+          _Dot(),
+          _Dot(),
+        ],
+      ),
+    );
+  }
+}
+
+class _Dot extends StatelessWidget {
+  const _Dot({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 12,
+      height: 12,
+      margin: EdgeInsets.symmetric(
+        horizontal: 4,
+      ),
+      decoration: BoxDecoration(
+        color: Colors.grey,
+        shape: BoxShape.circle,
+      ),
+    );
+  }
+}
+
+class _Slides extends StatelessWidget {
+  _Slides({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: PageView(
+        children: [
+          _Slide(
+            svgPath: 'assets/svgs/slide-1.svg',
+          ),
+          _Slide(
+            svgPath: 'assets/svgs/slide-2.svg',
+          ),
+          _Slide(
+            svgPath: 'assets/svgs/slide-3.svg',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  final String svgPath;
+
+  _Slide({
+    required this.svgPath,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: double.infinity,
+        width: double.infinity,
+        padding: EdgeInsets.all(30),
+        child: SvgPicture.asset(svgPath));
   }
 }
