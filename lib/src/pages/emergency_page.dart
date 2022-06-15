795e11b2ef7bce300432bf813301afd3d3af775b
Alejandro Fachini Colombo <alefachinic@Alejandros-MacBook-Pro.local>
2022-06-15 17:24:09 -0300

    FatButton customizable

--- lib/src/pages/emergency_page.dart
+++ lib/src/pages/emergency_page.dart
@@ -1,3 +1,4 @@
+import 'package:designs/src/widgets/fat_button.dart';
 import 'package:designs/src/widgets/headers.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -8,11 +9,32 @@ class EmergencyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: IconHeader(
-        icon: FontAwesomeIcons.plus,
-        subTilte: 'Haz solicitado',
-        title: 'Asistencia Médica',
+      body: Center(
+        child: FatButton(
+          icon: FontAwesomeIcons.carBurst,
+          text: 'Motor Accident',
+          colorGrad1: Color(0xff6989f5),
+          colorGrad2: Color(0xff906ef5),
+          onPress: (() {
+            print('Click!');
+          }),
+        ),
       ),
+    );
+  }
+}
+
+class PageHeader extends StatelessWidget {
+  const PageHeader({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return IconHeader(
+      icon: FontAwesomeIcons.plus,
+      subTilte: 'Haz solicitado',
+      title: 'Asistencia Médica',
     );
   }
 }
