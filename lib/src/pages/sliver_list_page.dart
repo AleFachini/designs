065d117cce4b9272c67082c7e8523c72f19d8913
Alejandro Fachini Colombo <alefachinic@Alejandros-MacBook-Pro.local>
2022-06-16 17:34:09 -0300

    Ñista y titulos separados listos para agregar al Sliver

--- lib/src/pages/sliver_list_page.dart
+++ lib/src/pages/sliver_list_page.dart
@@ -6,8 +6,105 @@ class SliverListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.amber,
+      body: _TaskList(), //_Title(),
+    );
+  }
+}
+
+class _Title extends StatelessWidget {
+  _Title({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          height: 30,
+        ),
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+          child: Text(
+            'New',
+            style: TextStyle(
+              color: Color(0xff532128),
+              fontSize: 50,
+            ),
+          ),
+        ),
+        Stack(
+          children: [
+            SizedBox(
+              width: 100,
+            ),
+            Positioned(
+              bottom: 8,
+              child: Container(
+                width: 150,
+                height: 8,
+                color: Color(0xfff7cdd5),
+              ),
+            ),
+            Container(
+              child: Text(
+                'List',
+                style: TextStyle(
+                  color: Color(0xffd93a30),
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
+
+class _TaskList extends StatelessWidget {
+  final items = [
+    _ListItem('Orange', Color(0xffF08F66)),
+    _ListItem('Family', Color(0xffF2A38A)),
+    _ListItem('Subscriptions', Color(0xffF7CDD5)),
+    _ListItem('Books', Color(0xffFCEBAF)),
+    _ListItem('Orange', Color(0xffF08F66)),
+    _ListItem('Family', Color(0xffF2A38A)),
+    _ListItem('Subscriptions', Color(0xffF7CDD5)),
+    _ListItem('Books', Color(0xffFCEBAF)),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: items.length,
+        itemBuilder: ((context, index) => items[index]));
+  }
+}
+
+class _ListItem extends StatelessWidget {
+  final String title;
+  final Color color;
+
+  const _ListItem(this.title, this.color); //argumentos posicionales
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.centerLeft,
+      padding: EdgeInsets.all(30),
+      height: 130,
+      margin: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(30),
+      ),
+      child: Text(
+        title,
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 20,
+        ),
       ),
     );
   }
